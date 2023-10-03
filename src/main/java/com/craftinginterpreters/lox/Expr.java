6f86a05664766805b3f34d9b93b956a58f1340a5
Danil Sidoruk <danilsidoruknv@yandex.ru>
2023-10-03 17:22:54 +0300

    Add definition of Set expression

--- src/main/java/com/craftinginterpreters/lox/Expr.java
+++ src/main/java/com/craftinginterpreters/lox/Expr.java
@@ -18,6 +18,8 @@ abstract class Expr {
 
     R visitLogicalExpr(Logical expr);
 
+    R visitSetExpr(Set expr);
+
     R visitUnaryExpr(Unary expr);
 
     R visitVariableExpr(Variable expr);
@@ -128,6 +130,23 @@ abstract class Expr {
     final Expr left;
     final Token operator;
     final Expr right;
+  }
+
+  static class Set extends Expr {
+    Set(Expr object, Token name, Expr value) {
+      this.object = object;
+      this.name = name;
+      this.value = value;
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitSetExpr(this);
+    }
+
+    final Expr object;
+    final Token name;
+    final Expr value;
   }
 
   static class Unary extends Expr {
